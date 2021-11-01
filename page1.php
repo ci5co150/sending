c355b41f32a27b807266c0845e78081f3da0f88d
Francisco G <74437512+ci5co150@users.noreply.github.com>
2021-11-01 10:11:36 -0700

    week10

---
+++ page1.php
@@ -0,0 +1,16 @@
+<?php
+$a_str=(isSet($_POST('a'))?$_POST['a']:[....]; // post
+//$a_str=(GET GET); //get
+
+for Array
+for Median
+for SD
+for Min Max
+
+$a = JSON_decode($a_str);
+
+call for $a
+
+$output = ['test']//['min' => min, 'max' => max, ....]
+   echo JSON_encode($output);
+?>
